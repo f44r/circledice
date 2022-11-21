32e985264975d2f855952da01f993c641a7ac5c9
hidfug <34993841+hidfug@users.noreply.github.com>
2022-11-21 14:57:10 +0800

    Update index.ts

--- plugins/dicetest/src/index.ts
+++ plugins/dicetest/src/index.ts
@@ -1,27 +1,54 @@
-import { Context, Schema } from 'koishi'
+import { Context } from 'koishi'
 
-export const name = 'dicetest'
-
-export interface Config {}
-
-export const Config: Schema<Config> = Schema.object({})
+export const name = 'circledice'
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  ctx.command('r')
+    .option('result', '-r [r] 骰点理由（可选）', {fallback: ""})
+    .option('dice', '-d [d] 骰点式（默认为1d100）', { fallback: "1d100"})
+    .option('check', '-c [c] 技能、武器鉴定(未实现)', {fallback: ""})
+    .action(({options}) => Dice_return(options))
 }
 
-export function test(ctx: Context) {
-  ctx.middleware((session, next) => {
-    const regex = /^test/i;
-    let flag = session.content.match(regex)
-    if ( flag 
-      && (session.userId === "360326608" || session.userId === "Alice")) {
-      return _test(session)
+function Dice_parsing(DiceString){
+  let patt = /([\(\)])|([+\-*\/])|(\d+)|([a-z])/ig
+  return DiceString.match(patt)
+}
+
+function Dice_d_random(time,range){
+  let r = 0;
+  for(let i = 0;i < time;i++){
+    r += Math.ceil(Math.random()*range)
+  }
+  return String(r)
+}
+
+function Dice_analyze(Arr){
+  let Dice_Number = new Array()
+  for(let i = 0 ; i < Arr.length-2 ; ){
+    if(String(Number(Arr[i])) != "NaN" && String(Number(Arr[i+2])) != "NaN"){
+      if(Arr[i+1] == "d" || Arr[i+1] == "D"){
+        Dice_Number = Arr.splice(i,3)
+	Arr.splice(i,0,Dice_d_random(Dice_Number[0],Dice_Number[2]))
+      }else{
+        i++
+      }
+    }else{
+      i++
     }
-    return next()
-  })
+  }
+  return Arr
 }
 
-function _test(obj:any){
-  return JSON.stringify(obj.content)
-}
+function Dice_return(Dice){
+  let Dice_Arr = Dice_parsing(Dice.dice)
+  Dice_Arr = Dice_analyze(Dice_Arr)
+  let pattErr = /[^0-9=\-*\/]/i
+  if(!pattErr.test(Dice_Arr.join(""))){
+    let fun = new Function("return " + Dice_Arr.join(""))
+    if(Dice_Arr.length > 2){
+      if(Dice.result == ""){
+        return Dice.dice + " = " + Dice_Arr.join("") + " = " + Math.ceil(fun())
+      }else{
+        return "因" + Dice.result + "进行的骰点结果为：" + Dice.dice + " = " + Dice_Arr.join("") + " = " + Math.ceil(fun())
+      }
