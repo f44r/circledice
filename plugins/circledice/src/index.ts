3eec2903ff63bf452cd5f253e11e6be857118e25
hidfug <34993841+hidfug@users.noreply.github.com>
2022-11-22 17:39:19 +0800

    Update index.ts

--- plugins/circledice/src/index.ts
+++ plugins/circledice/src/index.ts
@@ -1,10 +1,23 @@
-import { Context } from 'koishi'
+import { Context, Schema } from 'koishi'
 import * as Dice_r from './Dice_r'
 import * as Dice_st from './Dice_st'
 
 export const name = 'circledice'
 
-export function apply(ctx: Context) {
+export interface Config {
+  uploadPC: string
+}
+
+export const Config = Schema.object({
+  uploadPC: Schema.string().default(),
+})
+
+export function apply(ctx: Context, config: Config) {
   ctx.plugin(Dice_r)
   ctx.plugin(Dice_st)
+
+  ctx.command('uploadpc [message]')
+    .action((_, message) => {
+      _.session.onebot.uploadGroupFile(_.session.guildId, config.uploadPC, "COC7空白卡.xlsx")
+    })
 }
