0736d1f8569a64711d2dea1e51234f9e1df55295
Frostnova00 <113450984+Frostnova00@users.noreply.github.com>
2023-03-09 15:33:30 +0800

    Add files via upload

--- src/cmd.ts
+++ src/cmd.ts
@@ -12,14 +12,25 @@ export function apply(ctx: Context, config: Config) {
     .action(async (argv, skill) => {
       const { user, channel } = argv.session
       let ch = await circle.getCh(user, channel ? channel.gameSpace : null)
+	  let gamerule = channel.gameSpace.rule
+	  if(gamerule == undefined){
+		let gamerule = 'coc7/1'
+	  }
+
       log.info(ch.assets)
       if(!Number.isNaN(+ch.get(skill))){
-        //没写房规，小子
         let r = Math.ceil(Math.random()*100)
         let said = ch.name + '进行的' + skill + '鉴定为：' + r + '/' + ch.get(skill)
-        if(r == 100){
+        if((gamerule == 'coc7/1' && ((ch.get(skill) < 50 && r >95)||(ch.get(skill) >= 50 && r == 100)))||
+		   (gamerule == 'coc7/2' && (r == 100 || (r > 95 && r > ch.get(skill))))||
+		   (gamerule == 'coc7/3' && r > 95)
+		){
           said += '大失败'
-        }else if(r == 1){
+        }else if(
+			(gamerule == 'coc7/1' && ((ch.get(skill) < 50 && r == 1)||(ch.get(skill) >= 50 && r < 6)))||
+			(gamerule == 'coc7/2' && (r == 1 || (r < 6 && r < ch.get(skill))))||
+			(gamerule == 'coc7/3' && r < 6)
+		){
           said += '大成功'
         }else if(r > +ch.get(skill)){
           said += '失败'
@@ -35,6 +46,33 @@ export function apply(ctx: Context, config: Config) {
         //这是复杂skill，如枪械
       }
     })
+	
+  ctx.command('setrule [text]')
+    .channelFields(['gameSpace'])
+    .userFields(['id', 'player'])
+    .action(async (argv, text) => {
+      const { user, channel } = argv.session
+	  let said = ''
+		if(text == 'coc7/1' || text == 'COC7/1'){
+			channel.gameSpace.rule = 'coc7/1'
+			said += '已设置房间规则为coc7/1'
+		}
+		else if(text == 'coc7/2' || text == 'COC7/2'){
+			channel.gameSpace.rule = 'coc7/2'
+			said += '已设置房间规则为coc7/2'
+		}
+		else if(text == 'coc7/3' || text == 'COC7/3'){
+			channel.gameSpace.rule = 'coc7/3'
+			said += '已设置房间规则为coc7/3'
+		}
+		else{
+			said += '请输入正确的房间规则，如coc7/1'
+		}
+		await ctx.database.set('channel', channel.Id ,{gameSpace:channel.gameSpace})
+		return said
+	})
+
+	
   ctx.command('roll [text]')
     .action((_, text) => {
       return JSON.stringify(circle.roll(text))
