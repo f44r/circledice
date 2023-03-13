6f2b99925ef8bfbb5b6e386a0e4c15dbc60d4cbc
Frostnova00 <113450984+Frostnova00@users.noreply.github.com>
2023-03-13 13:16:51 +0800

    Add files via upload

--- src/cmd.ts
+++ src/cmd.ts
@@ -6,7 +6,7 @@ export const name = 'cmd'
 const log = new Logger('CircleDice/cmd:')
 
 export function apply(ctx: Context, config: Config) {
-  ctx.command('ra [skill]')
+	ctx.command('ra [skill]')
     .channelFields(['gameSpace'])
     .userFields(['id', 'player'])
     .action(async (argv, skill) => {
@@ -49,12 +49,134 @@ export function apply(ctx: Context, config: Config) {
         //这是复杂skill，如枪械
       }
     })
+	
+	ctx.command('rb [text]')
+    .action(async (argv ,text) => {
+		const { session } = argv
+		var num : string[]
+		var res : number[]
+		var num0 : number
+		var num1 : number
+		var freq : number
+		
+		if(Number(text)){
+			num = [Number(text)]
+		}
+		else{
+			num = text.match(/[0-9]+/g);  // 获取骰点表达式中全部的数字，储存在数组num中
+		}
+		
+		if(num == undefined){
+			num = ["1"]
+		}
+		
+		if(Number(num[0]) > 9 || Number(num[0]) < 0){
+			session.send("错误：奖励骰次数应为0-9之间的正整数")
+		}
+		else if(num[1] != undefined){
+			session.send("错误：检测到复数个奖励骰次数")
+		}
+		else{
+			freq = Number(num[0]) //freq为奖励骰次数
+			res = new Array(freq + 1)  
+			num0 = Math.ceil(Math.random() * 100) //确定个位数的第一次骰点
+			res[0] = (num0 - (num0 % 10)) / 10 
+			for (i = 1; i <= freq; i++) {
+				let r = Math.ceil(Math.random() * 100)
+				res[i] = (r - (r % 10)) / 10 //获取随机结果的十位数
+			}
+			
+			said = "奖励骰计算结果为1d100 = " + num0 + "[奖励骰:"
+			for (i = 1; i < res.length - 1; i++) {
+				said = said + res[i] + ","
+			}res.length - 1
+			said += res[res.length - 1]
+			
+			for (let i = 0; i < res.length; i++) {
+				for (let j = i + 1; j < res.length; j++) {
+					if (res[j] < res[i]) {
+					let temp = res[j];
+					res[j] = res[i];
+					res[i] = temp; //冒泡排序
+					}
+				}
+			}
+			
+			num1 = res[0] * 10 + num0 % 10 //num1为奖励骰最终计算结果
+			
 
-  ctx.command('roll [text]')
+			said = said + "]=" + num1 //said为发送信息
+			
+			session.send(said)
+		}
+	})
+	
+	ctx.command('rp [text]')
+    .action(async (argv ,text) => {
+		const { session } = argv
+		var num : string[]
+		var res : number[]
+		var num0 : number
+		var num1 : number
+		var freq : number
+		
+		if(Number(text)){
+			num = [Number(text)]
+		}
+		else{
+			num = text.match(/[0-9]+/g);  // 获取骰点表达式中全部的数字，储存在数组num中
+		}
+		
+		if(num == undefined){
+			num = ["1"]
+		}
+		
+		if(Number(num[0]) > 9 || Number(num[0]) < 0){
+			session.send("错误：奖励骰次数应为0-9之间的正整数")
+		}
+		else if(num[1] != undefined){
+			session.send("错误：检测到复数个奖励骰次数")
+		}
+		else{
+			freq = Number(num[0]) //freq为惩罚骰次数
+			res = new Array(freq + 1)  
+			num0 = Math.ceil(Math.random() * 100) //确定个位数的第一次骰点
+			res[0] = (num0 - (num0 % 10)) / 10 
+			for (i = 1; i <= freq; i++) {
+				let r = Math.ceil(Math.random() * 100)
+				res[i] = (r - (r % 10)) / 10 //获取随机结果的十位数
+			}
+			
+			said = "惩罚骰计算结果为1d100 = " + num0 + "[惩罚骰:"
+			for (i = 1; i < res.length - 1; i++) {
+				said = said + res[i] + ","
+			}res.length - 1
+			said += res[res.length - 1]
+			
+			for (let i = 0; i < res.length; i++) {
+				for (let j = i + 1; j < res.length; j++) {
+					if (res[j] < res[i]) {
+					let temp = res[j];
+					res[j] = res[i];
+					res[i] = temp; //冒泡排序
+					}
+				}
+			}
+			
+			num1 = res[res.length - 1] * 10 + num0 % 10 //num1为惩罚骰最终计算结果
+			
+
+			said = said + "]=" + num1 //said为发送信息
+			
+			session.send(said)
+		}
+	})
+	
+	ctx.command('roll [text]')
     .action((_, text) => {
       return JSON.stringify(circle.roll(text))
     })
-  ctx.command('save')
+	ctx.command('save')
     .action(async (_, text) => {
       let a = await ctx.database.create('circledice_pc', {
         'master': 0,
